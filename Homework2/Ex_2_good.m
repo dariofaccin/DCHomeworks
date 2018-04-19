9fa5b8cd105fbec293527b39c8a8d990415ea3d0
gio950 <33923557+gio950@users.noreply.github.com>
2018-04-19 23:41:30 +0200

    Ciao Grosso

--- Homework2/Ex_2_good.m
+++ Homework2/Ex_2_good.m
@@ -64,72 +64,71 @@ title('Impulse response of the channel')
 %% ESTIMATE OF THE PDF OF H_p=|h0|/sqrt(M)
 h_p = h0/sqrt(C^2+Md);        % the normalization here does not make much sense
                               % as M_h0=1-C^2, but it's to keep the formulas as in the book
-
-h_sd = std(abs(h_p));         % mean 
-h_mean = mean(abs(h_p));      % variance 
 abs_h = abs(h_p);             % magnitude
-
-x=linspace(0,3,3000);
-v = sqrt(K/(K+1));
-s = sqrt(1/(2*(K+1)));
+a=linspace(0,10,3000);
 % Rice distribution
-th_pdf = (x./s^2).*exp(-0.5*(x.^2+v.^2)./s^2).*besseli(0,x.*v./s^2);
+th_pdf = 2*(1+K).*a.*exp(-K-(1+K).*a.^2).*besseli(0,2.*a*sqrt(K*(1+K)));
 % Estimate of the pdf
 [y,t] = hist(abs_h);          
 est_pdf = y/max(y);
 
 figure
-bar(t,est_pdf,'g'), hold on,plot(x,th_pdf,'r--','LineWidth',2); 
+bar(t,est_pdf,'g'), hold on,plot(a,th_pdf,'r--','LineWidth',2); 
 ylabel('Number of samples')
 xlabel('Value')
-title('Histogram of $h_0$')
+title('Histogram of $\frac{|h_0|}{\sqrt{M_{|h_0|}}}$')
 legend('estimate pdf','theoretical pdf');
 axis([0 3 0 1.3]);
+grid on
 
 %% SPECTRUM ESTIMATION
+% Theoretical PSD
+rx = autocorrelation_Unb(h0(1:N_h0)'); 
+H0 = fft(rx);
+H0 = fftshift(H0);
+
 % Welch estimator
-S=20000;                     % overlap
-H_dopp=40000;                % window length
+S=1000;                     % overlap
+H_dopp=2000;                % window length
 w_welch=window(@hamming,H_dopp);
 %w_welch=kaiser(D,5);
-[Welch_P, N_t] = welchPSD(h0', w_welch, S);
-f=1/Tc:1/Tc:N_t;
+[Welch_P, N] = welchPSD(h0(1:N_h0)', w_welch, S);     % FAI SU TUTTO h0
+f=1/Tc:1/Tc:N;
+norm_f = f/N;
 Welch_centered=fftshift(Welch_P);
-figure,
-plot(10*log10(Welch_centered))
+
+figure
+plot(norm_f,10*log10(Welch_centered)), hold on
+plot(norm_f,10*log10(abs(H0)));
 ylim([-5 35])
-xlim([N_t/2-5*N_t*fd N_t/2+5*N_t*fd])
+xlim([0.5-5*fd  0.5+5*fd]);
+% xlim([N_t/2-5*N_t*fd N_t/2+5*N_t*fd])
 xticks([39850 39900 39950 40000 40050 40100 40150])
 xticklabels({'-150','-100','-50','0','50','100','150'});
 ylabel('H(f) [dB]')
-xlabel('f') 
+xlabel('f')
+legend('Welch Periodogram','Theoretical PSD')
 
 %%
-rx = autocorrelation_Unb(h0');
-H0 = fft(rx);
-H0 = fftshift(H0);
-figure(), plot(10*log10(abs(H0)))
-
 
 % Comparison of different S,D
-% S = [500 600 2000 5000];
-% D = [1000 1200 4000 10000];
-% for i=1:length(S)
-%     w_welch=window(@bartlett,D(i));
-%     [Welch_P(:,i), Ns] = welchPSD(h0', w_welch, S(i));
-%     var_Welch=Welch_P(:,i).^2/Ns;
-% end
-% f=1/Tc:1/Tc:N_t;
-% Welch_centered=fftshift(Welch_P);
-% % Welch_centered = Welch_P;
-% figure,
-% plot(10*log10(Welch_centered))
-% ylim([-10 30])
-% xlim([N_t/2-5*N_t*fd N_t/2+5*N_t*fd])
-% xticks([39850 39900 39950 40000 40050 40100 40150])
-% xticklabels({'-150','-100','-50','0','50','100','150'});
-% ylabel('H(f) [dB]'), xlabel('f')
-% legend(['D = ' int2str(D(1)) ' and S = ' int2str(S(1)) ], ['D = ' int2str(D(2)) ' and S = ' int2str(S(2)) ],['D = ' int2str(D(3)) ' and S = ' int2str(S(3)) ],['D = ' int2str(D(4)) ' and S = ' int2str(S(4)) ]);
+S = [500 600 2000 5000];
+D = [1000 1200 4000 10000];
+for i=1:length(S)
+    w_welch=window(@hamming,D(i));
+    [Welch_P(:,i), N] = welchPSD(h0(1:N_h0)', w_welch, S(i));
+end
+Welch_centered=fftshift(Welch_P);
+f=1/Tc:1/Tc:N;
+norm_f = f/N;
+
+figure,
+plot(norm_f,10*log10(Welch_centered)), hold on
+plot(norm_f,10*log10(abs(H0)),'r--');
+ylim([-5 35])
+xlim([0.5-5*fd  0.5+5*fd]);
+ylabel('H(f) [dB]'), xlabel('f')
+legend(['D = ' int2str(D(1)) ' and S = ' int2str(S(1)) ], ['D = ' int2str(D(2)) ' and S = ' int2str(S(2)) ],['D = ' int2str(D(3)) ' and S = ' int2str(S(3)) ],['D = ' int2str(D(4)) ' and S = ' int2str(S(4)) ]);
 
 
 
