5a906abed43a6320c7b00a829b2c3ee54c565e55
unknown <dario.fac@gmail.com>
2018-05-05 10:07:33 +0200

    Started HW3: mi uccido

---
+++ Homework3/Channel_gen.m
@@ -0,0 +1,31 @@
+clc; close all; clear global; clearvars;
+
+T = 1;
+Tc = T/4;
+
+Q = T/Tc;
+
+alpha = 0.67;
+beta = 0.7424;
+
+qc_num = [0 0 0 0 0 beta];
+qc_denom = [1 -alpha];
+qc = impz(qc_num, qc_denom);
+
+E_qc = sum(qc.^2);
+
+x = PNSeq(1023);
+
+x_i = x(1:ceil(length(x)/2)-1);
+x_q = x(ceil(length(x)/2)+1:end);
+
+a_prime_i = upsample(x_i, Q);
+a_prime_q = upsample(x_q, Q);
+
+a_prime = a_prime_i + 1i*a_prime_q;
+
+s_c = filter(qc_num, qc_denom, a_prime);
+
+noise = wgn(length(s_c),1,10);
+
+r_c = s_c + noise;
