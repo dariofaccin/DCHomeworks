171012d1c4cef708426786981c0d9b619dc27ddd
unknown <dario.fac@gmail.com>
2018-05-07 17:58:59 +0200

    Added input for all receivers.

--- Homework3/Channel_gen.m
+++ Homework3/Channel_gen.m
@@ -1,37 +1,38 @@
 clc; close all; clear global; clearvars;
 
+% MUST BE USED ONCE TO GENERATE AN INPUT FOR
+% ALL RECEIVERS
+
 T = 1;              % Symbol period
 Tc = T/4;           % upsampling period
 Q = T/Tc;           
+snr = 10;
+sigma_a = 2;
 
-alpha = 0.67;       % filter coefficients
+% Filter setup
+alpha = 0.67;
 beta = 0.7424;
-
-qc_num = [0 0 0 0 0 beta];       % qc filter
+qc_num = [0 0 0 0 0 beta];
 qc_denom = [1 -alpha];
 qc = impz(qc_num, qc_denom);
 E_qc = sum(qc.^2);
 
-x = PNSeq(1023);     % generate a strem of 0 and 1 
+% Input signal: from a PN sequence generate QPSK
+x = PNSeq(1023);
+in_bits = bitmap(x(1:length(x)-1));
 
-% x_i = x(1:ceil(length(x)/2)-1);
-% x_q = x(ceil(length(x)/2)+1:end);
+% Upsampling
+a_prime = upsample(in_bits,Q);
 
-x_i = x(1:2:end);
-x_q = x(2:2:end-1);
+% Noise
+sigma_w = sigma_a * E_qc / (4*snr);
 
-x_i = x_i(1:length(x_q));
-
-a_prime_i = upsample(x_i, Q);
-a_prime_q = upsample(x_q, Q);
-
-a_prime = a_prime_i + 1i*a_prime_q;
-
+% Output
 s_c = filter(qc_num, qc_denom, a_prime);
-
-noise = wgn(length(s_c),1,10);
-
+noise = wgn(length(s_c),1,sigma_w);
 r_c = s_c+noise;
+
+save('rec_input.mat', 'in_bits', 'r_c', 'qc', 'E_qc');
 
 %% FIGURES
 
@@ -47,5 +48,3 @@ xlim([1 length(abs_qc)]);
 figure()
 plot(f,10*log10(abs(Qc)/length(Qc)));
 xlim([0 2/T]);
-
-save('channel_output.mat', 'r_c');
--- Homework3/Receiver1.m
+++ Homework3/Receiver1.m
@@ -1,24 +1,16 @@
 clc; close all; clear global; clearvars;
 
-% Input signal PN sequence of even length
-in_sig = PNSeq(1023);
-in_sig = in_sig(1:length(in_sig)-1);
+% Input signal
+load('rec_input.mat');
 
-% Mapping PN -> QPSK
-in_bits = bitmap(in_sig);
-
-sigma_a = 2;
 snr_db = 10;
 snr_lin = 10^(snr_db/10);
-
-% Transmitter
-[ch_out, sigma_w, qc] = channel_sim(in_bits, snr_lin, sigma_a);
 
 % Matched filter
 q_mf = qc(end:-1:1);
 
 % Filtering through C
-y = filter(q_mf,1,ch_out);
+y = filter(q_mf,1,r_c);
 y = downsample(y,4);
 
 detected = zeros(length(y),1);
