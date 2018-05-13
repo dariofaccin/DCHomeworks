d46a1b38f2eee02cfbe688e037f133e4d3285900
gio950 <33923557+gio950@users.noreply.github.com>
2018-05-13 11:46:00 +0200

    G_AA & some stuff

--- Homework3/Receiver_c.m
+++ Homework3/Receiver_c.m
@@ -15,9 +15,63 @@ sigma_a = 2;
 s_c = r_c;                  % Useful noise
 r_c = r_c + w(:,3);
 
-figure
-subplot(211), plot(abs([fft(autocorr(s_c))));
-subplot(212), plot(abs(fft(crosscorr(s_c,s_c))));
+% Anti-Aliasing filter
+Fpass = 0.45;            % Passband Frequency
+Fstop = 0.55;            % Stopband Frequency
+Dpass = 0.057501127785;  % Passband Ripple
+Dstop = 0.0001;          % Stopband Attenuation
+dens  = 20;              % Density Factor
+% Calculate the order from the parameters using FIRPMORD.
+[N, Fo, Ao, W] = firpmord([Fpass, Fstop], [1 0], [Dpass, Dstop]);
+% Calculate the coefficients using the FIRPM function.
+g_AA  = firpm(N, Fo, Ao, W, {dens});
+% G_AA = dfilt.dffir(g_AA);
+
+% Filtering received signal
+r_c_prime = filter(g_AA,1,r_c);
+t0_bar = length(g_AA);
+
+% Remove "transient" and downsample received signal
+r_c_prime = r_c_prime(t0_bar:end);
+x = downsample(r_c_prime,2);
+
+% Matched filter
+g = conv(qc, g_AA);
+gm = conj(g(end:-1:1));
+gm = downsample(gm,2);
+
+% Impulse response of the system at the input of the FF filter
+h = conv(g,gm);
+h = h(h>max(h)/100);
+
+x = filter(gm,1,x);
+
+% Filter autocorrelation
+r_gm = xcorr(gm,gm);
+rw_tilde = sigma_w/4*r_gm;
+
+% Parameters for DFE
+M1 = 8;
+M2 = 2;
+D = 3;
+[c_opt, Jmin] = Adaptive_DFE(h, rw_tilde, sigma_a, M1, M2, D);
+
+psi = conv(c_opt, h_T);
+psi = psi/max(psi);
+
+b = - psi(end - M2 + 1:end);
+
+%% FIGURES
+[G_AA, f] = freqz(g_AA,1,1024,'whole');
+f = linspace(0,4,length(f));
+
+figure()
+plot(f,10*log10(abs(G_AA)));
+xlim([0 2]);
+ylim([-40 10]);
+xlabel('f'), grid on
+ylabel('$| G_{AA}(f) |$ $[dB]$')
+
 
 
 
