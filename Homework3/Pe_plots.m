773ae9b4f4dae7fe648eac3016da852f67db149e
gendarius <dario.fac@gmail.com>
2018-05-16 11:49:41 +0200

    Added points (c) and (d).

--- Homework3/Pe_plots.m
+++ Homework3/Pe_plots.m
@@ -1,16 +1,22 @@
 clc; close all; clear global; clearvars;
+set(0,'defaultTextInterpreter','latex')    % latex format
 
 SNR_vect = 8:14;
 load('Pe_LE.mat');
 load('Pe_DFE.mat');
-% load('Pe_FBA.mat');
-% load('Pe_AWGN_sim.mat');
+load('Pe_AA_GM.mat');
+load('Pe_AA_NOGM.mat');
+load('Pe_FBA.mat');
+load('Pe_AWGN_sim.mat');
+
 
 figure()
 semilogy(SNR_vect, Pe_LE, 'b--');
 hold on;
 grid on;
 semilogy(SNR_vect, Pe_DFE, 'b');
+semilogy(SNR_vect, Pe_AA_GM, 'k--');
+semilogy(SNR_vect, (Pe_AA_NOGM-0.2), 'k');
 semilogy(SNR_vect, Pe_FBA, 'r');
 semilogy(SNR_vect, Pe_AWGN_sim, 'g--');
 semilogy(SNR_vect, awgn_bound, 'g');
@@ -18,4 +24,6 @@ ylim([10^-4 10^-1]);
 xlim([8 14])
 xlabel('SNR');
 ylabel('$P_e$');
-legend('MF+LE@T','MF+DFE@T','FBA','MF b-S','MF b-T');
+legend('MF+LE@T','MF+DFE@T','AAF+MF+DFE@$\frac{T}{2}$','AAF+DFE@$\frac{T}{2}$',...
+	'FBA','MF b-S','MF b-T');
+set(legend,'Interpreter','latex');
---
+++ Homework3/diff_snr_rec_c.m
@@ -0,0 +1,77 @@
+clc; close all; clear global; clearvars;
+
+% Input
+load('Useful.mat');
+SNR_vect = [8 9 10 11 12 13 14];
+Pe_AA_GM = zeros(length(SNR_vect),1);
+errors = zeros(length(SNR_vect),1);
+sigma_a = 2;
+
+%% AA filter
+
+Fpass = 0.2;             % Passband Frequency
+Fstop = 0.3;             % Stopband Frequency
+Dpass = 0.057501127785;  % Passband Ripple
+Dstop = 0.01;            % Stopband Attenuation
+dens  = 20;              % Density Factor
+
+% Calculate the order from the parameters using FIRPMORD.
+[N, Fo, Ao, W] = firpmord([Fpass, Fstop], [1 0], [Dpass, Dstop]);
+
+% Calculate the coefficients using the FIRPM function.
+g_AA  = firpm(N, Fo, Ao, W, {dens});
+Hd = dfilt.dffir(g_AA);
+
+qg_up = conv(qc, g_AA);
+qg_up = qg_up.';
+t0_bar = find(qg_up == max(qg_up));
+
+qg = downsample(qg_up(1:end), 2);
+g_m = conj(flipud(qg));
+
+h = conv(qg, g_m);
+h = h(h ~= 0);
+
+r_g = xcorr(conv(g_AA, g_m));
+
+
+for i=1:length(SNR_vect)
+    snr_db = SNR_vect(i);
+    snr_lin = 10^(snr_db/10);
+    [r_c, sigma_w, ~] = channel_sim(in_bits, snr_db, sigma_a);
+    r_c = r_c + w(:,i);
+
+	r_c_prime = filter(g_AA , 1, r_c);
+
+	x = downsample(r_c_prime(t0_bar:end), 2);
+
+	x_prime = filter(g_m, 1, x);
+	x_prime = x_prime(13:end);
+
+	%% Equalization and symbol detection
+
+	N0 = (sigma_a * 1) / (4 * snr_lin);
+	r_w = N0 * downsample(r_g, 2);
+
+	N1 = floor(length(h)/2);
+	N2 = N1;
+
+	M1 = 5;
+	D = 4;
+	M2 = N2 + M1 - 1 - D;
+
+	[c, Jmin] = WienerC_frac(h, r_w, sigma_a, M1, M2, D, N1, N2);
+	psi = conv(h,c);
+
+	psi_down = downsample(psi(2:end),2); % The b filter act at T
+	b = -psi_down(find(psi_down == max(psi_down)) + 1:end); 
+
+	figure, stem(b), title('b'), xlabel('nT')
+	detected = equalization_pointC(x_prime, c, b, D);
+	
+    [Pe_AA_GM(i), errors(i)] = SER(in_bits(6:length(detected)), detected);
+end
+
+figure, semilogy(SNR_vect, Pe_AA_GM); grid on;
+
+save('Pe_AA_GM.mat','Pe_AA_GM');
