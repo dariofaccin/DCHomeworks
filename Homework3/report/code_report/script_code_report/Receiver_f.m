3839f8a263e7b0a5e9839e7a50c15731f4e101e4
gendarius <dario.fac@gmail.com>
2018-05-19 16:31:38 +0200

    Attenzione al FBA

--- Homework3/report/code_report/script_code_report/Receiver_f.m
+++ Homework3/report/code_report/script_code_report/Receiver_f.m
@@ -1,55 +1,40 @@
 clc; close all; clear global; clearvars;
-set(0,'defaultTextInterpreter','latex')    % latex format
+set(0,'defaultTextInterpreter','latex')
 
-% Input
+% Load input and noise
 load('Useful.mat');
 
 % Channel SNR
 snr_db = 10;
 snr_lin = 10^(snr_db/10);
 
-sigma_a = 2;
+sigma_a = 2;	% Input variance
 
-[r_c, sigma_w, qc] = channel_sim(in_bits, snr_db, sigma_a);
-
+[r_c, sigma_w, ~] = channel_sim(in_bits, snr_db, sigma_a);
 r_c = r_c + w(:,3);
 
-% Matched filter
-gm = conj(qc(end:-1:1));
+gm = conj(qc(end:-1:1));	% Matched filter: complex conjugate of qc
 
-% Impulse response
-h = conv(qc,gm);
-h = h(h>max(h)/100);
-h = h(3:end-2);
+h = conv(qc,gm);				% Impulse response
+t0_bar = find(h == max(h));		% Determining timing phase
+h_T = downsample(h,4);			% Downsampling impulse response
+r_c_prime = filter(gm,1,r_c);	% Filtering received signal
 
-% Downsampling impulse response
-h_T = downsample(h,4);
+r_c_prime = r_c_prime(t0_bar:end);	% Remove "transient"
+x = downsample(r_c_prime,4);		% Downsample received signal
 
-% Filtering received signal
-r_c_prime = filter(gm,1,r_c);
-
-% Determining timing phase
-t0_bar = length(gm);
-
-% Remove "transient" and downsample received signal
-r_c_prime = r_c_prime(t0_bar:end);
-x = downsample(r_c_prime,4);
-
-% Filter autocorrelation
-r_gm = xcorr(gm,gm);
+r_gm = xcorr(gm,gm);			% Filter autocorrelation
 rw_tilde = sigma_w/4 .* downsample(r_gm, 4);
 
 % Parameters for DFE
-M1 = 3;
-N2 = 2;
-D = 2;
+M1 = 5;
+N2 = floor(length(h_T)/2);
+D = 4;
 M2 = N2 + M1 - 1 - D;
 [c_opt, Jmin] = Adaptive_DFE(h_T, rw_tilde, sigma_a, M1, M2, D);
 
 psi = conv(c_opt, h_T);
 psi = psi/max(psi);
-
-b = zeros(M2,1);
 
 figure
 subplot(121), stem(0:length(c_opt)-1,abs(c_opt)), hold on, grid on
@@ -57,11 +42,11 @@ title('$|c|$'), xlabel('n');
 subplot(122), stem(0:length(psi)-1,abs(psi)), grid on
 title('$|\psi|$'), xlabel('n');
 
-y = conv(x, c_opt);
+y = conv(x, c_opt);		% Input for ForwardBackward
 y = y/max(psi);
 
-L1 = 2;
-L2 = 2;
+L1 = 0;
+L2 = 4;
 indexD = find(psi == max(psi));
 
 detected = FBA(y, psi(indexD-L1:indexD+L2), L1, L2);
